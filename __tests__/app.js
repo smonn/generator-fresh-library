2f18b6544704c450477b5642cf3b643d06c826a7
Simon Ingeson <simon.ingeson@gmail.com>
2018-05-29 15:50:20 -0400

    undo Usage: npm <command>
    where <command> is one of:
        access, adduser, bin, bugs, c, cache, completion, config,
        ddp, dedupe, deprecate, dist-tag, docs, doctor, edit,
        explore, get, help, help-search, i, init, install,
        install-test, it, link, list, ln, login, logout, ls,
        outdated, owner, pack, ping, prefix, profile, prune,
        publish, rb, rebuild, repo, restart, root, run, run-script,
        s, se, search, set, shrinkwrap, star, stars, start, stop, t,
        team, test, token, tst, un, uninstall, unpublish, unstar,
        up, update, v, version, view, whoami
    
    npm <command> -h     quick help on <command>
    npm -l           display full usage info
    npm help <term>  search for help on <term>
    npm help npm     involved overview
    
    Specify configs in the ini-formatted file:
        /Users/smonn/.npmrc
    or on the command line via: npm <command> --key value
    Config info can be viewed via: npm help config
    
    npm@5.6.0 /Users/smonn/.nvm/versions/node/v8.11.2/lib/node_modules/npm
    
    Did you mean one of these?
        c
        i

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,18 +4,39 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('generator-fresh-library:app', () => {
+  let prompts;
+
+  function run() {
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts(prompts);
+  }
+
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+    prompts = {
       name: 'test-library',
       description: 'My test library',
       license: 'MIT',
       stage: 'babel-stage-2',
       react: false,
       yarn: true
-    });
+    };
   });
 
-  it('creates files', () => {
+  it('creates files with yarn', async () => {
+    await run();
+
+    assert.file([
+      'package.json',
+      '.gitignore',
+      'src/index.js',
+      '__tests__/index.test.js'
+    ]);
+  });
+
+  it('creates files without yarn', async () => {
+    prompts.yarn = false;
+
+    await run();
+
     assert.file([
       'package.json',
       '.gitignore',
